a6d7fad4ff548a67549b6cf34c6f10ad2dc4bd2f
Zack-911 <muhammadbinasim471@gmail.com>
2025-12-16 14:36:15 +0500

    feat: add custom function to calculate required XP and integrate it into the message send command.

---
+++ src/custom_functions/required_xp.ts
@@ -0,0 +1,31 @@
+import { ForgeFunction, ArgType } from "@tryforge/forgescript";
+
+export default new ForgeFunction({
+  name: "getRequiredXp",
+  params: [
+    {
+      name: "level",
+      required: true,
+      type: ArgType.Number,
+      rest: false
+    },
+    {
+      name: "current_xp",
+      required: false,
+      type: ArgType.Number,
+      rest: false
+    }
+  ],
+  code: `
+    $return[$djsEval[  
+      const BASE_XP = 100;
+      const GROWTH_RATE = 1.25;
+
+      const xpForNextLevel = Math.floor(
+        BASE_XP * Math.pow(GROWTH_RATE, $env[level] - 1)
+      );
+    
+      Math.max(0, xpForNextLevel - $env[current_xp] || 0);
+  ]]
+  `
+})
