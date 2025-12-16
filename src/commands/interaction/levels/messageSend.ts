f2c1ba84fb265b792b54829c44724133a080a127
Zack-911 <muhammadbinasim471@gmail.com>
2025-12-16 15:16:10 +0500

    fix: Correct XP calculation after level up by subtracting required XP for the current level.

--- src/commands/interaction/levels/messageSend.ts
+++ src/commands/interaction/levels/messageSend.ts
@@ -13,7 +13,7 @@ export default new BaseCommand({
     $let[required_xp;$getRequiredXp[$get[level]]]
     $let[xp_to_level_up;$getRequiredXp[$get[level];$get[xp]]]
     $if[$get[xp_to_level_up]==0;
-      $setMemberVar[xp;$sub[$get[xp];$get[required_xp]];$authorID;$guildID]
+      $setMemberVar[xp;$sub[$get[xp];$get[xp_to_level_up]];$authorID;$guildID]
       $setMemberVar[level;$math[$get[level]+1];$authorID;$guildID]
       $reply[$channelID;$messageID;false]
       $userDisplayName[$authorID] Level Up! You're now level $math[$get[level]+1]
